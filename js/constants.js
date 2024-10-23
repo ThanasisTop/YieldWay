62c460b1218e1a09a86b75adb727442d2a7f89ab
Athanasios Toptsios <atoptsios@allweb.gr>
2024-10-23 15:49:03 +0300

    add constants

---
+++ js/constants.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  const OfficeInfos = [
+    { key: "telephone1", value: "+30 6979745936" },
+    { key: "telephone2", value: "+30 6983255149" },
+    { key: "email", value: "info@yieldway.gr" },
+    { key: "availableHours", value: "9:00 πμ - 9:00 μμ" }
+  ];
+  
+  const SocialLinks = [
+    { key: "facebook", value: "https://www.facebook.com/" },
+    { key: "instagram", value: "https://www.instagram.com" },
+    { key: "linkedIn", value: "https://www.linkedIn.com" }
+  ];
+
+  
+  OfficeInfos.forEach((item) => {
+	  document.getElementById(item.key).innerText = item.value
+	  item.key!='availableHours'?document.getElementById(item.key+'Footer').innerText = item.value:document.getElementById(item.key).innerText = item.value
+	  });
+  
+  SocialLinks.forEach((item) => {
+	  document.getElementById(item.key).href = item.value
+	  document.getElementById(item.key+'Footer').href = item.value;
+	  });
+  
+  
+  
+});
